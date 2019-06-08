02f00cdc5c90b96c4a02cdc204de66131d7b2ec2
unknown <bitomagira@gmail.com>
2019-06-08 21:38:24 +0200

    first commit

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-
-export function TodoItem(props){
-    return (
-        <li
-            style={{ color: "black", cursor: "pointer" }}
-        >
-            {props.description}
-        </li>
-    );
+import "../App.css";
+export function TodoItem(props) {
+  return (
+    <tr>
+      <td style={{ width: "10%" }}>
+        <input
+          type="checkbox"
+          checked={props.done}
+          onChange={() => props.changeDone(props.ind)}
+        />
+      </td>
+      <td style={{ width: "70%", textAlign: "left" }}>
+        <p style={{ color: props.done ? "green" : "red" }}>
+          {props.description}
+        </p>
+      </td>
+      <td style={{ width: "10%" }}>
+        <button onClick={() => props.deleteItem(props.ind)}>Delete</button>
+      </td>
+      <td style={{ width: "10%" }}>
+        <button onClick={() => props.updateItem(props.ind)}>Update</button>
+      </td>
+    </tr>
+  );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,43 +3,73 @@ import { AddItem } from "./AddItem";
 import { TodoItem } from "./TodoItem";
 
 export class TodoList extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            items: [
-                {
-                    description: "buy things",
-                    done: false
-                }
-            ]
-        };
-    }
-
-    onAddItem = description => {
-        let items = this.state.items;
-        items.push({
-            description, 
-            done: false
-        });
-        this.setState({ items });
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        {
+          description: "buy things",
+          done: true
+        }
+      ]
     };
+  }
 
-    render() {
-        return (
-            <div>
-                <AddItem onAddItem={this.onAddItem} />
-                <ul>
-                    {this.state.items.map((value, index) => {
-                        return (
-                            <TodoItem
-                                description={value.description}
-                                done={value.done}
-                                key={index}
-                            />
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
+  onAddItem = description => {
+    let items = this.state.items;
+    items.push({
+      description,
+      done: false
+    });
+    this.setState({ items });
+  };
+
+  changeDone = index => {
+    let newItems = this.state.items;
+    let item = this.state.items[index];
+    item.done = !item.done;
+    newItems[index] = item;
+    this.setState({ items: newItems });
+  };
+
+  deleteItem = index => {
+    let newItems = this.state.items;
+    newItems.splice(index, 1);
+    this.setState({ items: newItems });
+  };
+
+  updateItem = index => {
+    let newItems = this.state.items;
+    let item = this.state.items[index];
+    let newDescription = prompt("update item description", item.description);
+    item.description = newDescription;
+    item.done = false;
+    newItems[index] = item;
+    this.setState({ items: newItems });
+  };
+
+  render() {
+    return (
+      <div>
+        <AddItem onAddItem={this.onAddItem} />
+        <table className="todoitem">
+          <tbody>
+            {this.state.items.map((value, index) => {
+              return (
+                <TodoItem
+                  description={value.description}
+                  done={value.done}
+                  key={index}
+                  ind={index}
+                  changeDone={this.changeDone}
+                  deleteItem={this.deleteItem}
+                  updateItem={this.updateItem}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
